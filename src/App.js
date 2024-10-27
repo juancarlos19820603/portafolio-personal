7295d1d9fa53b40b503c846e0da5a50f3d723c33
Juan Carlos Londoño <juancarloslondono1982@gmail.com>
2024-10-26 19:48:58 -0500

    feat(Banner): add skills carousel to Banner component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
 import {Banner} from './components/Banner'
+import { Skills } from './components/Skills';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useRef } from 'react';
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Skills/>
 
     </div>
   );
